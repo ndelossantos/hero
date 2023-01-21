d9c8366df1abb97b2acdae2ad20e3dd75ca892ba
AIM <aim@AIMs-MacBook-Pro.local>
2023-01-21 18:02:51 +0800

    ey

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,19 @@ gulp.task("licenses", async function () {
   gulp
     .src("build/static/js/*chunk.js", { base: "./" })
     .pipe(
-      gap.prependText(`x`)
+      gap.prependText(`/*!
+
+* Black Dashboard React - v1.2.0
+
+* Product Page: https://www.creative-tim.com/product/black-dashboard-react
+* Copyright 2020 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/black-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/`)
     )
     .pipe(gulp.dest("./", { overwrite: true }));
 
@@ -14,7 +26,18 @@ gulp.task("licenses", async function () {
   gulp
     .src("build/index.html", { base: "./" })
     .pipe(
-      gap.prependText(`x`)
+      gap.prependText(`<!--
+* Black Dashboard React - v1.2.0
+
+* Product Page: https://www.creative-tim.com/product/black-dashboard-react
+* Copyright 2020 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/black-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+-->`)
     )
     .pipe(gulp.dest("./", { overwrite: true }));
 
@@ -22,7 +45,18 @@ gulp.task("licenses", async function () {
   gulp
     .src("build/static/css/*chunk.css", { base: "./" })
     .pipe(
-      gap.prependText(`x`)
+      gap.prependText(`/*!
+* Black Dashboard React - v1.2.0
+
+* Product Page: https://www.creative-tim.com/product/black-dashboard-react
+* Copyright 2020 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/black-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/`)
     )
     .pipe(gulp.dest("./", { overwrite: true }));
   return;
